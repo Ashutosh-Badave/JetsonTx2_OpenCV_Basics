8449ae692aa1cfa392561ad24742d2fd75453aaa
Ashutosh-Badave <badave007@gmail.com>
2020-08-31 15:39:45 +0200

    CU: Added code for image read, show and write

--- GaussianBlur_and_Gradient.cpp
+++ GaussianBlur_and_Gradient.cpp
@@ -11,10 +11,28 @@
 using namespace std;
 
 void GuassianSmooth() {
-    cv::Mat img;
+
 
 }
 
 int main() {
-    GuassianSmooth();
+    // Loading image from KITTI dataset
+    string image_path = "../Data/KITTI/Camera/Data/0000000000.png";
+    cv::Mat img;
+
+    img = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+
+    if (img.empty()) {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    string windowName = "Output";
+    cv::imshow(windowName, img);
+    int k = cv::waitKey(0); // Wait for a keystroke in the window
+    if (k == 's') {
+        cv::imwrite("KITTI.png", img);
+        std::cout << "Image is saved " << std::endl;
+    }
+    return 0;
 }
+
