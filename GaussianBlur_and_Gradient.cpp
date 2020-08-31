a30da996537310ac14589937b6171bb0bd9359ec
Ashutosh-Badave <badave007@gmail.com>
2020-08-31 15:06:27 +0200

    Feature: GaussianBlur and Intesity gradient for smoothing.

---
+++ GaussianBlur_and_Gradient.cpp
@@ -0,0 +1,20 @@
+//
+// Created by ashutosh on 31.08.20.
+//
+
+#include <iostream>
+#include <numeric>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+
+void GuassianSmooth() {
+    cv::Mat img;
+
+}
+
+int main() {
+    GuassianSmooth();
+}
