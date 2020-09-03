3881ba15117f2b1b9fca8fef4cb8233c71713814
Ashutosh-Badave <badave007@gmail.com>
2020-09-03 12:01:56 +0200

    CU: cornerharris and BCR adjustment functions are removed.

--- GaussianBlur_and_Gradient.cpp
+++ GaussianBlur_and_Gradient.cpp
@@ -8,6 +8,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
+#include "BCR_adjustment.hpp"
 
 
 using namespace std;
@@ -16,51 +17,6 @@ using namespace cv;
 Mat img, imgGray;
 string windowName;
 
-Mat gammaCorrection(const Mat &source, const double gamma_) {
-    CV_Assert(gamma_ >= 0);
-    //! [changing-contrast-brightness-gamma-correction]
-    Mat lookUpTable(1, 256, CV_8U);
-    uchar *p = lookUpTable.ptr();
-    for (int i = 0; i < 256; ++i)
-        p[i] = saturate_cast<uchar>(pow(i / 255.0, gamma_) * 255.0);
-
-    Mat res = source.clone();
-    LUT(source, lookUpTable, res);
-    //! [changing-contrast-brightness-gamma-correction]
-    Mat img_gamma_corrected;
-    hconcat(source, res, img_gamma_corrected);
-    //imshow("Gamma correction", img_gamma_corrected);
-    return res;
-}
-
-Mat Brightness_and_Contrast() {
-    double contrast = 3; // between 1.0 - 3.0
-    double brightness = 100; // between 0 - 100
-    double gamma_c = 10;
-    Mat new_image = Mat::zeros(img.size(), img.type());
-
-    for (int row = 0; row < img.rows; row++) {
-        for (int col = 0; col < img.cols; col++) {
-            for (int chnl = 0; chnl < img.channels(); chnl++) {
-                new_image.at<Vec3b>(row, col)[chnl] = saturate_cast<unsigned char>(
-                        img.at<Vec3b>(row, col)[chnl] * contrast + brightness);
-            }
-        }
-    }
-    // Visulalization
-    windowName = "Contrast and Brightness";
-    //imshow(windowName, new_image);
-
-    Mat img_gammaC = gammaCorrection(new_image, gamma_c);
-
-    int k = cv::waitKey(0); // Wait for a keystroke in the window
-
-    if (k == 's') {
-        imwrite("../Output/After_contrastNbrightness.png", img_gammaC);
-        cout << "Image is saved " << std::endl;
-    }
-    return img_gammaC;
-}
 
 void GuassianSmooth(Mat &img, Mat &result) {
     // Created 5by5 discrete filter for gaussian smooth with standard deviation of 1
@@ -120,83 +76,6 @@ void GradientSobelFilter(Mat &Gblurred, Mat &result_sobel) {
 
 }
 
-void NMS_local(Mat &dst_norm, Mat &dst_norm_scaled, int &minResponse, int &apertureSize) {
-    vector<KeyPoint> keypoints;
-    double maxOverlap = 0.0;
-
-    for (int row = 0; row < dst_norm.rows; row++) {
-        for (int col = 0; col < dst_norm.cols; col++) {
-
-            int Response = static_cast<int>(dst_norm.at<float>(row, col));
-
-            if (Response > minResponse) {
-                KeyPoint Newkeypoint;
-                Newkeypoint.pt = Point2f(col, row);
-                Newkeypoint.size = 2 * apertureSize;
-                Newkeypoint.response = Response;
-
-                bool overlap = false;
-                for (auto it = keypoints.begin(); it != keypoints.end(); ++it) {
-                    double KPOverlap = KeyPoint::overlap(Newkeypoint, *it);
-                    if (KPOverlap > maxOverlap) {
-                        overlap = true;
-                        if (Newkeypoint.response > (*it).response) {
-                            *it = Newkeypoint;
-                            break;
-                        }
-                    }
-
-                }
-                if (!overlap) {
-                    keypoints.push_back(Newkeypoint);
-                }
-            }
-
-        }
-    }
-
-    // visualize keypoints
-    windowName = "NHS Results";
-    Mat visImage = dst_norm_scaled.clone();
-    drawKeypoints(dst_norm_scaled, keypoints, visImage, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    imshow(windowName, visImage);
-    int k = cv::waitKey(0); // Wait for a keystroke in the window
-
-    if (k == 's') {
-        imwrite("../Output/NHS_after_BCR_adujustment.png", visImage);
-        cout << "Image is saved after NHS" << std::endl;
-    }
-
-}
-
-void cornerHarris_detector() {
-    // Detector parameters
-    int blockSize = 2; // for every pixel, a blockSize × blockSize neighborhood is considered
-    int apertureSize = 3; // aperture parameter for Sobel operator (must be odd)
-    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
-    double k = 0.04; // Harris parameter (see equation for details)
-
-    // Detect Harris corners and normalize output
-    Mat dst, dst_norm, dst_norm_scaled;
-    dst = cv::Mat::zeros(imgGray.size(), CV_32FC1);
-    cornerHarris(imgGray, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
-    //normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
-    normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX);
-    convertScaleAbs(dst_norm, dst_norm_scaled);
-
-    // visualize results
-    windowName = "Harris Corner Detector Response Matrix";
-    imshow(windowName, dst_norm_scaled);
-    int h = cv::waitKey(0); // Wait for a keystroke in the window
-
-    if (h == 'h') {
-        imwrite("../Output/HarrisD_after_BCR_adujustment.png", dst_norm_scaled);
-        cout << "Image is saved " << std::endl;
-    }
-
-    NMS_local(dst_norm, dst_norm_scaled, minResponse, apertureSize);
-}
-
 
 int main() {
     // Loading image from KITTI dataset
@@ -208,7 +87,11 @@ int main() {
         cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
-    Mat img_CNB = Brightness_and_Contrast();
+    double contrast = 3; // between 1.0 - 3.0
+    double brightness = 100; // between 0 - 100
+    double gamma_c = 10;
+
+    Mat img_CNB = Brightness_and_Contrast(img, brightness, contrast, gamma_c);
     //convert to grayscale
     cvtColor(img_CNB, imgGray, COLOR_BGR2GRAY);
 
@@ -218,7 +101,7 @@ int main() {
     GuassianSmooth(imgGray, result);
     string windowName = "Gaussian Window";
     imshow(windowName, result);
-
+*/
     // Use inbuld gaussian blur function with 5by5 kernel for comparision
     GaussianBlur(imgGray, Gblurred, {5, 5}, 2);
     windowName = "Gaussian Window";
@@ -234,11 +117,8 @@ int main() {
     // Sobel operator
     Mat result_sobel;
     GradientSobelFilter(Gblurred, result_sobel);
-*/
-    // Harris corner detector
-    cornerHarris_detector();
 
-    return 0;
+
 }
 
 
